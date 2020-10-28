68aec1f2096e8f9937973597e64377d04b289272
Marc Respass <marc@iliosinc.com>
2020-10-28 17:00:20 -0400

    Added SwiftUI objects for CoreData Add entitlements for cloud kit

--- UltimatePortfolio/UltimatePortfolioApp.swift
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -9,9 +9,18 @@ import SwiftUI
 
 @main
 struct UltimatePortfolioApp: App {
+    @StateObject var dataController: DataController
+
+    init() {
+        let dataController = DataController()
+        _dataController = StateObject(wrappedValue: dataController)
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, self.dataController.container.viewContext)
+                .environmentObject(self.dataController)
         }
     }
 }
