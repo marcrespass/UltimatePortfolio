f0133466b0c056b0e30d3149485f59a6173ed241
Marc Respass <marc@iliosinc.com>
2020-11-04 14:44:53 -0500

    Investigated AppStorage demonstrated why AppStorage is the wrong choice for this app (it is app-wide) and changed to SceneStorage

--- UltimatePortfolio/ContentView.swift
+++ UltimatePortfolio/ContentView.swift
@@ -7,22 +7,35 @@
 
 import SwiftUI
 
+/*
+ The problem with optionals
+we want selectedView to be an optional String so that it can be nil
+ when we start but now SwiftUI is comparing Optional<String> to String
+ and they can never be equal (!)
+ Thus we will change our static tags to be optional and set a value
+ and now they can be compared
+ */
 struct ContentView: View {
+    @SceneStorage("selectedView") var selectedView: String?
+
     var body: some View {
-        TabView {
+        TabView(selection: $selectedView) {
             HomeView()
+                .tag(HomeView.tag)
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
                 }
 
             ProjectsView(showClosedProjects: false)
+                .tag(ProjectsView.openTag)
                 .tabItem {
                     Image(systemName: "list.bullet")
                     Text("Open")
                 }
 
             ProjectsView(showClosedProjects: true)
+                .tag(ProjectsView.closedTag)
                 .tabItem {
                     Image(systemName: "checkmark")
                     Text("Closed")
