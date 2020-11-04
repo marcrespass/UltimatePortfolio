93a92ff72249e289316adcf05760c6182caa79e0
Marc Respass <marc@iliosinc.com>
2020-11-04 14:11:02 -0500

    Added CoreDataHelpers extensions to CoreData generated Item and Project Convert to use new convenience methods

---
+++ UltimatePortfolio/Item+CoreDataHelpers.swift
@@ -0,0 +1,34 @@
+//
+//  Item+CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Marc Respass on 11/4/20.
+//
+
+import CoreData
+
+extension Item {
+    static var example: Item {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+
+        let item = Item(context: viewContext)
+        item.title = "Example Item"
+        item.detail = "This is an example item"
+        item.priority = 3
+        item.creationDate = Date()
+        return item
+    }
+
+    var itemTitle: String {
+        title ?? ""
+    }
+
+    var itemDetail: String {
+        detail ?? ""
+    }
+
+    var itemCreationDate: Date {
+        creationDate ?? Date()
+    }
+}
