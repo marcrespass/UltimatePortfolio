45209d86d0b72c607040a08da6874d5fad187ed0
Marc Respass <marc@iliosinc.com>
2021-01-23 17:35:44 -0500

    create sample data, delete a project, test that the project and its items got deleted

--- UltimatePortfolioTests/ProjectTests.swift
+++ UltimatePortfolioTests/ProjectTests.swift
@@ -26,4 +26,19 @@ class ProjectTests: BaseTestCase {
         XCTAssertEqual(self.dataController.count(for: Project.fetchRequest()), targetCount)
         XCTAssertEqual(self.dataController.count(for: Item.fetchRequest()), (targetCount * targetCount))
     }
+
+    func testDeletingProjectCascadeDeletesItems() throws {
+        try dataController.createSampleData()
+
+        let request = NSFetchRequest<Project>(entityName: "Project")
+        let projects = try self.moc.fetch(request)
+
+        print(self.dataController.count(for: Project.fetchRequest()))
+        print(self.dataController.count(for: Item.fetchRequest()))
+
+        dataController.delete(projects[0])
+
+        XCTAssertEqual(self.dataController.count(for: Project.fetchRequest()), 4)
+        XCTAssertEqual(self.dataController.count(for: Item.fetchRequest()), 40)
+    }
 }
