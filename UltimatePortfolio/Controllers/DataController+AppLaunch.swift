a8364d276bd94a69fa78d1124b7d6a474dc7864b
Marc Respass <marc@iliosinc.com>
2021-06-03 13:07:12 -0400

    skip the app review when in DEBUG config

--- UltimatePortfolio/Controllers/DataController+AppLaunch.swift
+++ UltimatePortfolio/Controllers/DataController+AppLaunch.swift
@@ -14,11 +14,15 @@ extension DataController {
     func appLaunched() {
         guard count(for: Project.fetchRequest()) >= 5 else { return }
 
+        #if DEBUG
+        print("DEBUG CONFIG - OMITTING APP REVIEW")
+        #else
         let allScenes = UIApplication.shared.connectedScenes
         let scene = allScenes.first { $0.activationState == .foregroundActive }
 
         if let windowScene = scene as? UIWindowScene {
             SKStoreReviewController.requestReview(in: windowScene)
         }
+        #endif
     }
 }
