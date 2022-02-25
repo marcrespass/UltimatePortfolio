c93dceb846d4cc35499918ccb7cb81400670a0d4
Marc Respass <marc@iliosinc.com>
2022-02-25 12:26:18 -0500

    Added this CloudError

--- UltimatePortfolio/Extensions/CloudError.swift
+++ UltimatePortfolio/Extensions/CloudError.swift
@@ -15,4 +15,30 @@ struct CloudError: Identifiable, ExpressibleByStringInterpolation {
     init(stringLiteral value: String) {
         self.message = value
     }
+
+    init(error: Error) {
+        if let error = error as? CKError {
+            switch error.code {
+                case .badContainer, .badDatabase, .invalidArguments:
+                    self.message = "A fatal error \(error.code) occurred: \(error.localizedDescription)"
+
+                case .networkFailure, .networkUnavailable, .serverResponseLost, .serviceUnavailable:
+                    self.message = "There was a problem communicating with iCloud; please check your network connection and try again."
+
+                case .notAuthenticated:
+                    self.message = "There was a problem with your iCloud account; please check that you're logged in to iCloud."
+
+                case .requestRateLimited:
+                    self.message = "You've hit iCloud's rate limit; please wait a moment then try again."
+
+                case .quotaExceeded:
+                    self.message = "You've exceeded your iCloud quota; please clear up some space then try again."
+
+                default:
+                    self.message = "An unknown error \(error.code) occurred: \(error.localizedDescription)"
+            }
+        } else {
+            self.message = "An unknown error occurred: \(error.localizedDescription)"
+        }
+    }
 }
