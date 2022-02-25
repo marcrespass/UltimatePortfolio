3e7b9f5e420967990c9a8e094010e3d77d9a97c4
Marc Respass <marc@iliosinc.com>
2022-02-25 13:44:34 -0500

    add new calculated property

--- UltimatePortfolio/Extensions/CloudError.swift
+++ UltimatePortfolio/Extensions/CloudError.swift
@@ -5,12 +5,17 @@
 //  Created by Marc Respass on 9/27/21.
 //
 
+import SwiftUI
 import CloudKit
 import Foundation
 
 struct CloudError: Identifiable, ExpressibleByStringInterpolation {
     var id: String { message }
     var message: String
+
+    var localizedMessage: LocalizedStringKey {
+        LocalizedStringKey(message)
+    }
 
     init(stringLiteral value: String) {
         self.message = value
